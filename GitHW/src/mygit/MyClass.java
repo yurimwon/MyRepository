23cae85fa8702a2f9f1e1df3e742b8c5414c117c
Admin <Admin@KOSCOMK-M189TAI>
2023-12-14 20:48:45 +0900

    Add a println line.

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version");
 	}
 
 }
